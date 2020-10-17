b12cf8e800fef571402a439a65dfd66ec6240a69
ThaGrza <andrewgriswold7@gmail.com>
2020-10-16 19:02:22 -0500

    small changes for bootstrap and popper.js

--- src/Moviejeeves.js
+++ src/Moviejeeves.js
@@ -3,13 +3,29 @@ import './Moviejeeves.css';
 import Dropdown from 'react-bootstrap/DropdownButton';
 
 
+class Moviejeeves extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      platform: '',
+      dropdown: false
+    };
+  }
+
+  platformChange(event){
+    this.setState({platform: event.target.value})
+  };
+  
+  platformChange(event){
+    this.setState({platform: event.target.value})
+  };
 
 
-function Moviejeeves() {
-  return (
+  render(){
+    return(
     <div className='movie_container'>
-      <div className='genre_selectContainer'>
-        <Dropdown id="dropdown-basic-button"  onClick className='genre_button' title="Genre">
+      <div className="buttons_container">
+        <Dropdown id="dropdown-basic-button" className='genre_button' title="Genre">
           <Dropdown.Item href="/">Action</Dropdown.Item>
           <Dropdown.Item href="/">Comedy</Dropdown.Item>
           <Dropdown.Item href="/">Drama</Dropdown.Item>
@@ -18,25 +34,19 @@ function Moviejeeves() {
           <Dropdown.Item href="/">Animated</Dropdown.Item>
           <Dropdown.Item href="/">Romantic Comedy</Dropdown.Item>
         </Dropdown>
-      </div>
-
-
-
-      <div className='=platform_selectContainer'>
-        <Dropdown id="dropdown-basic-button" onClick className='genre_button' title="Platform">
+        <Dropdown id="dropdown-basic-button" className='genre_button' title="Platform">
           <Dropdown.Item href="#/action-1">Netflix</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Hulu</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Disney+</Dropdown.Item>
           <Dropdown.Item href="#/action-3">HBO</Dropdown.Item>
         </Dropdown>
       </div>
-
-      <div className='movie_buttonContainer'>
+      <div className="button_container">
         <button className='find_movieButton'>Big Red Button</button>
       </div>
-
     </div>
-  )
+    )
+  }
 }
 
-export default Moviejeeves
+export default Moviejeeves;
