af1c66b3532b4d3a978222d033b12dc85b913d9b
ThaGrza <andrewgriswold7@gmail.com>
2020-10-19 20:08:52 -0500

    fixed dropdown buttons

--- src/Moviejeeves.js
+++ src/Moviejeeves.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Moviejeeves.css';
-import Dropdown from 'react-bootstrap/DropdownButton';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 
 class Moviejeeves extends React.Component{
@@ -16,16 +17,15 @@ class Moviejeeves extends React.Component{
     this.setState({platform: event.target.value})
   };
   
-  platformChange(event){
-    this.setState({platform: event.target.value})
-  };
-
+  // platformChange(event){
+  //   this.setState({platform: event.target.value})
+  // };
 
   render(){
     return(
     <div className='movie_container'>
       <div className="buttons_container">
-        <Dropdown id="dropdown-basic-button" className='genre_button' title="Genre">
+        <DropdownButton id="dropdown-basic-button" className='genre_button' title="Genre">
           <Dropdown.Item href="/">Action</Dropdown.Item>
           <Dropdown.Item href="/">Comedy</Dropdown.Item>
           <Dropdown.Item href="/">Drama</Dropdown.Item>
@@ -33,13 +33,13 @@ class Moviejeeves extends React.Component{
           <Dropdown.Item href="/">Thriller</Dropdown.Item>
           <Dropdown.Item href="/">Animated</Dropdown.Item>
           <Dropdown.Item href="/">Romantic Comedy</Dropdown.Item>
-        </Dropdown>
-        <Dropdown id="dropdown-basic-button" className='genre_button' title="Platform">
+        </DropdownButton>
+        <DropdownButton id="dropdown-basic-button" className='genre_button' title="Platform">
           <Dropdown.Item href="#/action-1">Netflix</Dropdown.Item>
           <Dropdown.Item href="#/action-2">Hulu</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Disney+</Dropdown.Item>
           <Dropdown.Item href="#/action-3">HBO</Dropdown.Item>
-        </Dropdown>
+        </DropdownButton>
       </div>
       <div className="button_container">
         <button className='find_movieButton'>Big Red Button</button>
