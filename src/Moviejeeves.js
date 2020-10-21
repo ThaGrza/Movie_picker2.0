94aac529b6e5b08f2f057e81dd5d9ebd6817862e
ThaGrza <andrewgriswold7@gmail.com>
2020-10-20 21:58:49 -0500

    small changes to buttons

--- src/Moviejeeves.js
+++ src/Moviejeeves.js
@@ -9,40 +9,48 @@ class Moviejeeves extends React.Component{
     super(props)
     this.state = {
       platform: '',
-      dropdown: false
+      genre: '',
+      search: ''
     };
   }
+
+  genreChange(event){
+    this.setState({genre: event.target.value})
+  };
 
   platformChange(event){
     this.setState({platform: event.target.value})
   };
+
+  searchChange(event){
+    this.setState({search: event.target.value})
+    console.log(this.state)
+    console.log(" IS THIS BUTTON WORKING"); 
+  }
   
-  // platformChange(event){
-  //   this.setState({platform: event.target.value})
-  // };
 
   render(){
     return(
     <div className='movie_container'>
       <div className="buttons_container">
-        <DropdownButton id="dropdown-basic-button" className='genre_button' title="Genre">
-          <Dropdown.Item href="/">Action</Dropdown.Item>
-          <Dropdown.Item href="/">Comedy</Dropdown.Item>
+        <DropdownButton id="dropdown-basic-button" onChange={this.genreChange.bind(this)} className='genre_button' title="Genre">
+          <Dropdown.Item>Action</Dropdown.Item>
+          <Dropdown.Item>Comedy</Dropdown.Item>
           <Dropdown.Item href="/">Drama</Dropdown.Item>
           <Dropdown.Item href="/">Horror</Dropdown.Item>
           <Dropdown.Item href="/">Thriller</Dropdown.Item>
           <Dropdown.Item href="/">Animated</Dropdown.Item>
           <Dropdown.Item href="/">Romantic Comedy</Dropdown.Item>
         </DropdownButton>
-        <DropdownButton id="dropdown-basic-button" className='genre_button' title="Platform">
-          <Dropdown.Item href="#/action-1">Netflix</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Hulu</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Disney+</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">HBO</Dropdown.Item>
+        <DropdownButton id="dropdown-basic-button" onChange={this.platformChange.bind(this)} className='genre_button' title="Platform">
+          <Dropdown.Item id="Netflix" href="/">Netflix</Dropdown.Item>
+          <Dropdown.Item href="/">Hulu</Dropdown.Item>
+          <Dropdown.Item href="/">Disney+</Dropdown.Item>
+          <Dropdown.Item href="/">HBO</Dropdown.Item>
         </DropdownButton>
       </div>
       <div className="button_container">
-        <button className='find_movieButton'>Big Red Button</button>
+        <button className='find_movieButton' onClick={this.searchChange.bind(this)} >GIVE ME A MOVIE</button>
       </div>
     </div>
     )
