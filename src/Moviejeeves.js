8c9aaee58c2be4d2ecb629a947a19b1a38e8157c
ThaGrza <andrewgriswold7@gmail.com>
2020-11-02 22:56:59 -0600

    pivoting to randmon movie generator, api lacks search features

--- src/Moviejeeves.js
+++ src/Moviejeeves.js
@@ -3,18 +3,14 @@ import './Moviejeeves.css';
 import Axios from 'axios';
 require('dotenv').config();
 
-
-
-
 class Moviejeeves extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      platform: '',
-      genre: '',
       movieDisplay: '',
       movieImg: '',
-      movieName: ''
+      movieName: '',
+      movieImageNotFound: 'https://media1.tenor.com/images/ef4c232dab28b7581497cee047f21969/tenor.gif?itemid=5521264'
     };
     this.genreChange = this.genreChange.bind(this);
     this.platformChange = this.platformChange.bind(this);
@@ -42,10 +38,14 @@ class Moviejeeves extends React.Component{
   getMovie(query){
     Axios.get(query)
       .then(res => {
-        console.log(res);
-        this.setState({movieDisplay: true});
         this.setState({movieName: res.data.title})
         this.setState({movieImg: res.data.backdrop_path})
+        console.log(res);
+        if (res.data.backdrop_path === null){
+          this.setState({movieDisplay: false});
+        }else{
+          this.setState({movieDisplay: true});
+        }
       })
       .catch(err => {
         if(err.response.status === 404){
@@ -64,39 +64,15 @@ class Moviejeeves extends React.Component{
   render(){
     return(
       <div className='movie_container'>
-        <div className='buttons_container'>
-          <select className='genre_button' onChange={this.genreChange}>
-            <option defaultValue=''>Genre</option>
-            <option value='28'>Action</option>
-            <option value='12'>Adventure</option>
-            <option value='16'>Animation</option>
-            <option value='35'>Comedy</option>
-            <option value='80'>Crime</option>
-            <option value='99'>Documentary</option>
-            <option value='18'>Drama</option>
-            <option value='10751'>Family</option>
-            <option value='14'>Fantasy</option>
-            <option value='36'>History</option>
-            <option value='27'>Horror</option>
-            <option value='10402'>Music</option>
-            <option value='9648'>Mystery</option>
-            <option value='10749'>Romance</option>
-            <option value='878'>Science Fiction</option>
-            <option value='53'>Thriller</option>
-            <option value='10752'>War</option>
-            <option value='37'>Western</option>
-          </select>
-          <select className='genre_button' onChange={this.platformChange}>
-            <option defaultValue=''>Platform</option>
-            <option value='netflix'>Netflix</option>
-            <option value='hulu'>Hulu</option>
-            <option value='disney'>Disney+</option>
-            <option value='hbo'>HBO</option>
-          </select>
-        </div>
         {this.state.movieDisplay === true && <div className='movie_display'>
           <span className='movie_title'>{this.state.movieName}</span>
-          <img src={'https://image.tmdb.org/t/p/w500/' + this.state.movieImg} className='movie_image' alt='Image Not Found'></img>
+          <img src={'https://image.tmdb.org/t/p/w500/' + this.state.movieImg} className='movie_image' alt='Found'/>
+        </div>
+        }
+        {this.state.movieDisplay === false && <div className='movie_display'>
+          <span className='movie_title'>{this.state.movieName}</span>
+          <span className='movie_image_NotFound'>Couldn't Find an Image</span>
+          <img src={this.state.movieImageNotFound} className='movie_image' alt='Not Found'/>
         </div>
         }
         <div className='button_container'>
